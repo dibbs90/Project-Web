fdedaa1075d60d17a04456406b1cf012d33e453b
dibbs90 <dibbsvcanary@gmail.com>
2019-07-23 23:57:30 +1000

    Add files via upload

--- content.js
+++ content.js
@@ -1,2 +1,9 @@
 document.write("What a Day! Lets do some Arithmatics")
-alert("Dont Click on the OK")
+    /*alert("Dont Click on the OK")
+     */
+function summation() {
+    let num1 = document.getElementById("#num1")[0].value);
+let num2 = document.getElementById("#num2")[0].value);
+let sum = number(num1) + Number(num2);
+document.getElementById("sum").value = sum;
+}
