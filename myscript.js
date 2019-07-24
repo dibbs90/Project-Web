4315aa35b8ddb4207d862b7f5dc314cbcd12c2e2
Dibyendu <dibbsvcanary@gmail.com>
2019-07-24 19:35:11 +0530

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -4,21 +4,26 @@ function add() {
     var a = document.getElementById("num1").value;
     var b = document.getElementById("num2").value;
     var c = Number(a) + Number(b);
-    
+
     // var a = 10;
     // var b = 34;
     // var c = 0;
     // var c = a + b;
     document.getElementById("sum").value = c;
+}
+
+function mltadd() {
+    var a = document.getElementById("nms").value;
+    var arr = a.split(",");
+    var s = 0;
+    for (let i = 0; i < arr.length; i++) {
+        var s = s + Number(arr[i]);
     }
 
-    function mltadd() {
-        var a= document.getElementById("nms").value;
-        var arr = a.split(",");
-        var s=0;
-        for (let i = 0; i < arr.length; i++) {
-            var s=s+Number(arr[i]);      
-        }
-         
-        document.getElementById("tot1").value=s;
-    }
+    document.getElementById("tot1").value = s;
+}
+
+function sort() {
+    alert("I'm not ready yet");
+
+}
