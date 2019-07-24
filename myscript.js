695fe9de339bc327663eb8908954a4c8c4f04992
dibbs90 <dibbsvcanary@gmail.com>
2019-07-24 17:13:13 +0530

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,9 +1,13 @@
 // this for the arithmetic functions
 
 function add() {
-    // var a = document.getElementById("num1").value);
-    // var b = document.getElementById("num2").value);
-    // var c = parseFloat(a) + parseFloat(b);
-    // document.getElementById("sum").value = c;
-    alert("Welcome");
-}
+    var a = document.getElementById("num1").value;
+    var b = document.getElementById("num2").value;
+    var c = Number(a) + Number(b);
+    
+    // var a = 10;
+    // var b = 34;
+    // var c = 0;
+    // var c = a + b;
+    document.getElementById("sum").value = c;
+    }
